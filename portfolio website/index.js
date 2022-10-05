e85a98d067b35aebad47ae9cbaed0ef4501c7e2c
JenMcD-star <jenniferannvancura@gmail.com>
2022-10-05 14:19:46 -0500

    Created loop function to input skills list. Added styling for list items

--- portfolio website/index.js
+++ portfolio website/index.js
@@ -3,6 +3,15 @@ window.onload = function () {
   document.getElementById("year").innerHTML = "© Jennifer McDougall " + year;
 };
 
+const skills = ['JavaScript', 'HTML', 'CSS', 'Problem Solving', 'Effective Communication']
+
+skills.forEach(function(item) {
+  let li = document.createElement("li");
+  let text = document.createTextNode(item);
+  li.appendChild(text);
+  document.getElementById("skillsList").appendChild(li);
+});
+
 const nametext = document.getElementById("nametext");
 const emailtext = document.getElementById("emailtext");
 const messagetext = document.getElementById("messagetext");
